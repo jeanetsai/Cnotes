e6aa67fc965a2b5eec83d460b74b1f4230b3a0eb
jeanetsai <52280399+jeanetsai@users.noreply.github.com>
2020-07-29 23:01:57 +0800

    Update sizeof.c

--- sizeof.c
+++ sizeof.c
@@ -6,6 +6,6 @@ printf("char\t%lu\n",sizeof(char));
 printf("int\t%lu\n",sizeof(int));
 printf("float\t%lu\n",sizeof(float));
 printf("double\t%lu\n",sizeof(double));
-/*output:1 4 4 8 */
+/*output:1 4 4 8 ptr+1 若為char則移動1 int則移動4 float則移動4 double移動8*/
 }
 
