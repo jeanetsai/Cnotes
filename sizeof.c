2ccb3a65d45992a0c5db3af832d9f93a5b6cb0e9
jeanetsai <52280399+jeanetsai@users.noreply.github.com>
2020-07-29 22:59:32 +0800

    Add files via upload

---
+++ sizeof.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+printf("char\t%lu\n",sizeof(char));
+printf("int\t%lu\n",sizeof(int));
+printf("float\t%lu\n",sizeof(float));
+printf("double\t%lu\n",sizeof(double));
+/*output:1 4 4 8 */
+}
+
