f68ee24946baf5b2d563b59214e30b4d6e907907
jeanetsai <52280399+jeanetsai@users.noreply.github.com>
2020-07-30 21:55:03 +0800

    Add files via upload

---
+++ 1_ptrarray2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+int twos[5]= {2, 4, 6, 8, 10};
+int *pt;
+
+
+pt=twos;
+printf("%d\n", *(pt+0));
+printf("%d\n", *(pt+1));
+printf("%d\n", *(pt+2));
+printf("%d\n", *(pt+3));
+printf("%d\n", *(pt+4));
+}
+
+/*output:
+2
+4
+6
+8
+10
+array[n]==*(ptr+n)
+*/
+
+
