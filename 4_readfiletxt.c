1af07176154198a9c492bacf996caef116237b02
jeanetsai <52280399+jeanetsai@users.noreply.github.com>
2020-07-30 22:25:34 +0800

    Add files via upload

---
+++ 4_readfiletxt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	
+	const char filename[] = "file.txt";
+	FILE *fh;
+	char buffer[64];	
+	/* open the file */
+	fh = fopen(filename, "r");
+		if (fh == NULL){
+			printf("Unable to read from file %s \n",filename);
+			return(1);
+		}
+	
+	printf("File '%s' opened \n", filename);
+	
+	/* read the file 64 characters at a time */
+	while( fgets(buffer,64,fh) )
+		printf("%s", buffer);
+	
+	
+	/* close when you're done*/
+	fclose(fh);
+	
+	return(0);
+	
+/* Output:
+File 'file.txt' opened	
+This is a book.
+That is a pen.*/
+
+}
